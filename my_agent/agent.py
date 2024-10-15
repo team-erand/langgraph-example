e6d926354730ef7e38e9a97d9ee63aa5c833caa9
Harrison Chase <hw.chase.17@gmail.com>
2024-10-15 08:22:31 -0700

    cr

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -1,52 +1,115 @@
-from typing import TypedDict, Literal
+from langgraph.graph import StateGraph, END, START, MessagesState
+from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
 
-from langgraph.graph import StateGraph, END
-from my_agent.utils.nodes import call_model, should_continue, tool_node
-from my_agent.utils.state import AgentState
 
+@tool
+def get_weather(city: str):
+    """Get the weather for a specific city"""
+    return f"It's sunny in {city}!"
+
+
+raw_model = ChatOpenAI()
+model = raw_model.with_structured_output(get_weather)
+
+
+class SubGraphState(MessagesState):
+    city: str
+
+
+def model_node(state: SubGraphState):
+    result = model.invoke(state["messages"])
+    return {"city": result["city"]}
+
+
+def weather_node(state: SubGraphState):
+    result = get_weather.invoke({"city": state["city"]})
+    return {"messages": [{"role": "assistant", "content": result}]}
+
+
+subgraph = StateGraph(SubGraphState)
+subgraph.add_node(model_node)
+subgraph.add_node(weather_node)
+subgraph.add_edge(START, "model_node")
+subgraph.add_edge("model_node", "weather_node")
+subgraph.add_edge("weather_node", END)
+subgraph = subgraph.compile(interrupt_before=["weather_node"])
 
 # Define the config
-class GraphConfig(TypedDict):
-    model_name: Literal["anthropic", "openai"]
+from typing import Literal
+from typing_extensions import TypedDict
+from langgraph.checkpoint.memory import MemorySaver
 
 
-# Define a new graph
-workflow = StateGraph(AgentState, config_schema=GraphConfig)
+memory = MemorySaver()
 
-# Define the two nodes we will cycle between
-workflow.add_node("agent", call_model)
-workflow.add_node("action", tool_node)
 
-# Set the entrypoint as `agent`
-# This means that this node is the first one called
-workflow.set_entry_point("agent")
+class RouterState(MessagesState):
+    route: Literal["weather", "other"]
 
-# We now add a conditional edge
-workflow.add_conditional_edges(
-    # First, we define the start node. We use `agent`.
-    # This means these are the edges taken after the `agent` node is called.
-    "agent",
-    # Next, we pass in the function that will determine which node is called next.
-    should_continue,
-    # Finally we pass in a mapping.
-    # The keys are strings, and the values are other nodes.
-    # END is a special node marking that the graph should finish.
-    # What will happen is we will call `should_continue`, and then the output of that
-    # will be matched against the keys in this mapping.
-    # Based on which one it matches, that node will then be called.
-    {
-        # If `tools`, then we call the tool node.
-        "continue": "action",
-        # Otherwise we finish.
-        "end": END,
-    },
+
+class Router(TypedDict):
+    route: Literal["weather", "other"]
+
+
+router_model = raw_model.with_structured_output(Router)
+
+
+def router_node(state: RouterState):
+    system_message = "Classify the incoming query as either about weather or not."
+    messages = [{"role": "system", "content": system_message}] + state["messages"]
+    route = router_model.invoke(messages)
+    return {"route": route["route"]}
+
+
+def normal_llm_node(state: RouterState):
+    response = raw_model.invoke(state["messages"])
+    return {"messages": [response]}
+
+
+def route_after_prediction(
+    state: RouterState,
+) -> Literal["weather_graph", "normal_llm_node"]:
+    if state["route"] == "weather":
+        return "weather_graph"
+    else:
+        return "normal_llm_node"
+
+
+graph = StateGraph(RouterState)
+graph.add_node(router_node)
+graph.add_node(normal_llm_node)
+graph.add_node("weather_graph", subgraph)
+graph.add_edge(START, "router_node")
+graph.add_conditional_edges("router_node", route_after_prediction)
+graph.add_edge("normal_llm_node", END)
+graph.add_edge("weather_graph", END)
+graph = graph.compile()
+
+
+
+class GrandfatherState(MessagesState):
+    to_continue: bool
+
+
+def router_node(state: GrandfatherState):
+    # Dummy logic that will always continue
+    return {"to_continue": True}
+
+
+def route_after_prediction(state: GrandfatherState):
+    if state["to_continue"]:
+        return "graph"
+    else:
+        return END
+
+
+grandparent_graph = StateGraph(GrandfatherState)
+grandparent_graph.add_node(router_node)
+grandparent_graph.add_node("graph", graph)
+grandparent_graph.add_edge(START, "router_node")
+grandparent_graph.add_conditional_edges(
+    "router_node", route_after_prediction, ["graph", END]
 )
-
-# We now add a normal edge from `tools` to `agent`.
-# This means that after `tools` is called, `agent` node is called next.
-workflow.add_edge("action", "agent")
-
-# Finally, we compile it!
-# This compiles it into a LangChain Runnable,
-# meaning you can use it as you would any other runnable
-graph = workflow.compile()
+grandparent_graph.add_edge("graph", END)
+grandparent_graph = grandparent_graph.compile()
